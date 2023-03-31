ba1d0201c2c422fa4b9b55e7cc6f8259a1fcd672
jose torres <andytorres25995@gmail.com>
2023-03-31 00:20:45 -0400

    added selectchar and passlength variables

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ var generateBtn = document.querySelector("#generate");
 const passwordCharacterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const passwordCharacterUp = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Q", "P", "Q", "R", "S", "T", "U", "'V", "W", "X", "Y", "Z"];
 const passwordCharacterSym = ["~", "`","!"," @", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\ ", ":", ";","'", "<", ",", ">", ".", "?", "/"];
-let passwordCharacterNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
+const passwordCharacterNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const selectedCharacters = ""
+const passLength = "0"
 // Write password to the #password input
 function writePassword() {
   
-  var length = prompt("Please choose a length (Between 8-128)");
-  var low =  confirm("Do you want to include lowercase")
-  var upper = confirm("Do you want to include uppercase")
- var symbol = confirm("Do you want to include symbols")
- var number = confirm("Do you want to include numbers")
+  let length = prompt("Please choose a length (Between 8-128)");
+  let low =  confirm("Do you want to include lowercase")
+  let upper = confirm("Do you want to include uppercase")
+ let symbol = confirm("Do you want to include symbols")
+ let number = confirm("Do you want to include numbers")
   if (!length) {
    return
   } else  if(length > 128) {
@@ -25,12 +26,26 @@ function writePassword() {
   if (length >= 8) {
    low()
   } 
-  if (low == true) {
+  if (low = true) {
     
     upper()
   } else {
     
     upper()
+  }
+  if (upper = true) {
+    
+    symbol()
+  } else {
+    
+    symbol()
+  }
+  if (symbol = true) {
+    
+    number()
+  } else {
+    
+    number()
   }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");

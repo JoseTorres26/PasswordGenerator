c41242fe9061e43f2f1a1cda5c1f5590a361ec29
jose torres <andytorres25995@gmail.com>
2023-04-03 21:46:36 -0400

    added concats

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const passwordCharacterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
 const passwordCharacterUp = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Q", "P", "Q", "R", "S", "T", "U", "'V", "W", "X", "Y", "Z"];
 const passwordCharacterSym = ["~", "`","!"," @", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\ ", ":", ";","'", "<", ",", ">", ".", "?", "/"];
 const passwordCharacterNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const selectedCharacters = ""
-const passLength = "0"
+const selectedCharacters = ["0"]
+
 // Write password to the #password input
 function writePassword() {
   let length = prompt("Please choose a length (Between 8-128)");
@@ -22,51 +22,52 @@ function writePassword() {
     }
     
   if (length >= 8) {
-    
-
-   
+   console.log(length);
   } 
   if (confirm("Do you want to include lowercase?") ) {
     console.log("true");
-   
-    
+   selectedCharacters.concat(passwordCharacterLow)
+    console.log(selectedCharacters);
   } else  {
     console.log("false");
   
   }
   if (confirm("Do you want to include uppercase?")) {
     console.log("true")
-    
-  
+    selectedCharacters.concat(passwordCharacterUp)
+    console.log(selectedCharacters);
   } else {
     console.log("false")
     
   }
   if (confirm("Do you want to include symbols?")) {
     console.log("true");
-    
-    
+    selectedCharacters.concat(passwordCharacterSym)
+    console.log(selectedCharacters);
   } else {
     console.log("false")
     
-  }
-if (confirm("Do you want to include numbers?")) {
+  } 
+  if (confirm("Do you want to include numbers?")) {
   console.log("true");
+  selectedCharacters.concat(passwordCharacterNum)
+  console.log(selectedCharacters);
 } else {
   console.log("false")
-}
-   
+}    
+let passLength = Number(length);
+    console.log(passLength);
+function generatePassword() {
+  for (let i = 0; i < passLength; i++) {
+    const pass = selectedCharacters[Math.floor(Math.random() * passLength)];
+     }
+     console.log(generatePassword());
+    };
   
-  
- 
- 
- 
- 
-  var password = generatePassword();
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 };
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 var element = this.EventTarget;

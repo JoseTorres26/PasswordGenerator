fe354b425b1d67632ef0ab2fcb4f4d68df0ed734
jose torres <andytorres25995@gmail.com>
2023-04-04 18:01:49 -0400

    added a readme and comments to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+ // variables for user input
 const passwordCharacterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const passwordCharacterUp = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Q", "P", "Q", "R", "S", "T", "U", "'V", "W", "X", "Y", "Z"];
 const passwordCharacterSym = ["~", "`","!"," @", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\ ", ":", ";","'", "<", ",", ">", ".", "?", "/"];
 const passwordCharacterNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let selectedCharacters = [""]
 
-// Write password to the #password input
+// user promts to determine length and characters to include
 function writePassword() {
   let length = prompt("Please choose a length (Between 8-128)");
   if (!length) {
    return writePassword;
+    // makes sure password is within specified range
   } else  if(length > 128) {
     alert("please choose no more than 128 characters.");
     return;
@@ -44,10 +45,12 @@ num = confirm("Do you want to include numbers?")
   selectedCharacters = selectedCharacters.concat(passwordCharacterNum);
   console.log(selectedCharacters);
      }
+      // if statement to make sure something is included
      if ((!low) && (!up) && (!sym) && (!num)){
-      alert("please use something")
+      alert("please use at least one set of characters!")
       return
-     }
+     } 
+      //randomizes characters and returns a password
      function generatePassword(){
       let passLength = Number(length);
       let passRand = "";
@@ -63,8 +66,9 @@ num = confirm("Do you want to include numbers?")
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+   // to reset value to make sure password is not reusing values
   selectedCharacters = [""]
   }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-var element = this.EventTarget;
+
